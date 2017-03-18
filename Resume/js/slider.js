d051076b11016094232c1ea48c736c17294595d9
liumochi <m18846089770@163.com>
2017-03-18 22:09:42 +0800

    我的个人网站

--- Resume/js/slider.js
+++ Resume/js/slider.js
@@ -1,8 +1,38 @@
 /**
  * Created by Administrator on 2017/3/15.
  */
-$(function(){
-   $('.da-arrows-next').on('click',function(){
+//var row = document.getElementsByClassName('row');
+//var iNow = 0;
+//var oPrev = document.getElementsByClassName('da-arrows-prev');
+//var oNext = document.getElementsByClassName('da-arrows-next');
+//oNext.onclick = function(){
+//   console.log(111);
+//   row[iNow].className='hide';
+//   iNow++;
+//   if(iNow==row.length){
+//      iNow=0;
+//   }
+//   row[iNow].className='show';
+//
+//};
+//oPrev.onclick = function(){
+//   row[iNow].className='hide';
+//   iNow--;
+//   if(iNow==-1){
+//      iNow=row.length-1
+//   }
+//};
 
+
+
+
+var iNow = 0;
+var $next = $('.da-arrows-next');
+var $text = $('.intro-text');
+var $text2 = $('.intro-text2');
+$(function(){
+   $next.on('click',function(){
+      $text.hide('hide');
+      $text2.fadeIn();
    })
 });
